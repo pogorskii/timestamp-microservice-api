cb88af3e5a0eac167ff541c24087f3341203a760
Stanislav <stanislav.pogorskii@gmail.com>
2023-10-24 15:41:47 +0400

    passed all tests

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// index.js
-// where your node app starts
-
 // init project
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require("cors");
+const cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -18,18 +15,34 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// your first API endpoint...
-app.get("/api/:date", (req, res) => {
-  const date = req.params.date;
-  const unix = Date.parse(date);
-  const utc = new Date(date).toUTCString();
-  res.json({
-    unix: unix,
-    utc: utc,
-  });
+// API endpoint
+// Handle if data string is empty
+app.get("/api/", (req, res) => {
+  const date = new Date();
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+// Handle if data string isn't empty
+app.get("/api/:date?", (req, res) => {
+  const inputDate = req.params.date;
+  let dateObj;
+
+  // Check if data is in Unix format
+  if (/\D/.test(inputDate)) {
+    dateObj = new Date(inputDate);
+  } else {
+    dateObj = new Date(parseInt(inputDate));
+  }
+
+  // Check if input data is valid
+  if (isNaN(dateObj.getTime())) {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({ unix: dateObj.getTime(), utc: dateObj.toUTCString() });
+  }
+});
+
+// Listen for requests
+const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
